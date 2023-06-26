25585ef40fcf3f8c03511a96b2c5948ec1a6450f
Britto <linuxwindows739@gmail.com>
2023-06-27 02:38:39 +0600

    Its working

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import './App.css';
 
 function App() {
   return (
+
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1>react is awesome</h1>
+        <h1>react is working from linux</h1>
         <p>
          Hello Everyone
         </p>
