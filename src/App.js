49056993ac383ac1596f1dee173d3ab16085d48a
Kowshik Haldar <haldarkowshik@gmail.com>
2023-06-27 02:12:42 +0600

    h1 p1 edited

--- src/App.js
+++ src/App.js
@@ -6,17 +6,11 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <h1>react is awesome</h1>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+         Hello Everyone
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
       </header>
     </div>
   );
